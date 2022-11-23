acc8c8547936e5826eb8662c38b5dacd9f564ce6
Paige Jenkins <paigemjenkins28@gmail.com>
2022-11-23 15:06:49 -0500

    wrote repeatString()

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string,num) {
+    let stringValue = '';
+    let count = 0;
+    
+    while (count < num) {
+        stringValue += string;
+        count++;
+    }
+    return stringValue;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
