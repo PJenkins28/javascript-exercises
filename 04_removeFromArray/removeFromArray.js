c69aa073f9640af079510dbd730ddb8467bcf9a8
Paige Jenkins <paigemjenkins28@gmail.com>
2022-11-24 02:11:18 -0500

    wrote function and ran first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(...items) {
+    const myArray = items[0];
+    const newArray = [];
 
+    myArray.forEach((i) => {
+        if(!items.includes(i)) {
+            newArray.push(i);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
