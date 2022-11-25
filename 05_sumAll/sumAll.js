99b69caabde3045b93f4b3c1afd38c917ebfef40
Paige Jenkins <paigemjenkins28@gmail.com>
2022-11-25 16:38:14 -0500

    wrote initial sumAll() and passed first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    let sum = 0;
+    if(num1 < num2) {
+        for (let i = num1; i <= num2; i++)
+        sum += i;
+    }
+    else if (num1 > num2) {
+        for (let i = num2; i <= num1; i++) 
+        sum += i;
+    }
+    else {
+        sum = num1 + num2;
+    }
+    return sum;
 };
 
 // Do not edit below this line
